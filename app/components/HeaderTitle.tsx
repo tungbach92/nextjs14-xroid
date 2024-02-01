f78340e86adee147e5a2770b4d238b0cfda5cad9
tungbach92 <tungbach@classfunc.com>
2024-02-01 18:02:04 +0700

    note

--- app/components/HeaderTitle.tsx
+++ app/components/HeaderTitle.tsx
@@ -1,4 +1,5 @@
 export default function HeaderTitle() {
+    // NODE_ENV = 'development' or 'production' when running even we don't include in env
     const title = process.env.NODE_ENV === "production" ? 'Xroid Studio β' : '[dev] Xroid Studio β'
     console.log(title)
     return (
